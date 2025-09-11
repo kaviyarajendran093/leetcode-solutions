98ca279b46b211ada28e4df62e773b62d3448dfe
kaviyarajendran093 <kaviya.r@ihealthwellness.org>
2025-09-11 17:00:25 -0400

    add insertion sort to the sortings

--- sortings/insertionSort.js
+++ sortings/insertionSort.js
@@ -1,4 +1,11 @@
 function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let j = i;
+    while (arr[j - 1] > arr[j]) {
+      [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]];
+      j--;
+    }
+  }
   return arr;
 }
 
